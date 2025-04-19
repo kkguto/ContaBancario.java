968938e90ab070f2a2f19d6bea2602ff3b6ad057
kkguto <guto050506@gmail.com>
2025-04-19 15:54:28 -0300

    App 1.0

---
+++ Banco/src/application/App.java
@@ -0,0 +1,87 @@
+package application;
+
+import entities.Account;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int escolha;
+        Account account = null;
+
+        do{
+            Menu();
+            escolha = sc.nextInt();
+            String password = null;
+            switch (escolha) {
+                case 1:
+                    account = CreateAccount(sc);
+                    break;
+                case 2:
+                    if (account == null) {
+                        System.out.println("No account created yet!");
+                        break;
+                    }
+
+                    System.out.print("Type the PassWord: ");
+                    password = sc.next();
+
+                    account.WithDrawMoney(password, sc);
+                    break;
+                case 3:
+                    if (account == null) {
+                        System.out.println("No account created yet!");
+                        break;
+                    }
+
+                    System.out.print("Type the amount you want to deposit: ");
+                    double amount = sc.nextDouble();
+
+                    account.DepositMoney(amount);
+                    break;
+                case 4:
+                    if (account == null) {
+                        System.out.println("No account created yet!");
+                        break;
+                    }
+
+                    System.out.print("Type the PassWord: ");
+                    password = sc.next();
+
+                    System.out.println(account.ShowAccountInfo(password));
+                    break;
+                case 5:
+                    System.out.println("App closed!");
+                    sc.close();
+                    break;                    
+                default:
+                    System.out.println("[ERRO] Option Invalid! Try Again.");
+                    break;
+            }
+        }while(escolha != 5);
+
+        
+    }
+    
+    static Account CreateAccount(Scanner sc){
+        
+        System.out.print("Type the name of Account Holder: ");
+        String holder = sc.next();
+        System.out.print("Type the PassWord: ");
+        String password = sc.next();
+
+        return new Account(holder, password);
+    }
+
+    static void Menu(){
+        System.out.println("\n======== MENU ========");
+        System.out.println("======================");
+        System.out.println("[1] Create Account");
+        System.out.println("[2] Withdraw Amount");
+        System.out.println("[3] Deposit Amount");
+        System.out.println("[4] Show Account Information");
+        System.out.println("[5] Exit");
+        System.out.println("======================");
+        System.out.print("Enter your choice: ");
+    }   
+}
---
+++ Banco/src/entities/Account.java
@@ -0,0 +1,90 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Account {
+    private String holder;
+    private String password;
+    private double balance;
+
+    //Constructor
+    public Account(){
+        this.holder = "No named";
+        this.password = "";
+        this.balance = 0.0;
+    }
+
+    public Account(String n, String p){
+        this.holder = n;
+        this.password = p;
+    }
+
+    //Methods
+    public void WithDrawMoney(String password, Scanner sc){
+        if(this.password.compareTo(password) == 0){ 
+            System.out.print("Type the amount you want to Withdraw: ");
+            double amount = sc.nextDouble();
+
+            if(this.balance >= amount){
+                this.balance -= amount;
+                System.out.println("Amount successfully WithDraw!");
+            }else{
+                System.out.println("Insufficient funds!");
+            } 
+        }else{
+            System.out.println("Wrong password!");
+        }
+
+    }
+
+    public void DepositMoney(double value){
+        if(value > 0){
+            this.balance += value;
+            System.out.println("Amount successfully Deposited into Account!");
+        }else{
+            System.out.println("[ERRO] Deposit attempt with Negative Numbers! Try again with numbers greater than zero.\n");
+        }
+
+    }
+
+    public String ShowAccountInfo(String password){
+        if(this.password.compareTo(password) == 0){
+
+            return "\nAccess allowed\n==========================\nAccount Holder: " + holder +
+                   "\nAmount: R$ " + String.format("%.2f", balance);
+        }
+
+        return "Access Denied";
+    }
+
+    public String toString(){
+        return  "\nAccount Holder: " + holder;
+    }
+
+    //Getters and Setters
+
+    //Account Holder
+    public String getHolder(){
+        return holder;
+    }
+
+    public void setHolder(String h){
+        this.holder = h;
+    }
+
+    //Password
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String p){
+        this.password = p;
+    }
+
+    //Value or Amount
+    public double getBalance(){
+        return balance;
+    }
+    //Just the getter because i want to they access the amount with only the methods
+
+}
